b1ba18dd88041bc6d60398c52088a20bee7bd325
Luiscef <enriquefranco2003@gmail.com>
2025-12-02 18:04:22 -0600

    databse

---
+++ lib/firebase_config.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+
+
+const firebaseConfig = FirebaseOptions(
+  apiKey: "AIzaSyBKYT5qq32YM8DuOp_A6z3auzdgBFJGNxY",
+  authDomain: "control-habitos.firebaseapp.com",
+  projectId: "control-habitos",
+  storageBucket: "control-habitos.firebasestorage.app",
+  messagingSenderId: "1058689194132",
+  appId: "1:1058689194132:web:f6a9d9d1a4edfe0ec77c81"
+);
